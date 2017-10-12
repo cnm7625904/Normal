7b8b56441ad49d220dfdbefc3c52c37d51c82a46
cnm7625904 <451083622@qq.com>
2017-10-12 15:25:50 +0800

    Initial commit

---
+++ app/src/main/java/jljt/wangs/com/testt/MainActivity.java
@@ -0,0 +1,13 @@
+package jljt.wangs.com.testt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
